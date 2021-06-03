a528f046e4dd0f2bcff6c57b96fc4b6833fe9c9f
Daniel Zvara <dan.zvara@gmail.com>
2021-06-03 13:58:07 +0200

    Hotfix xss 2

--- src/components/cell.tsx
+++ src/components/cell.tsx
@@ -127,7 +127,6 @@ export const Cell = React.memo(function(props: CellProps) {
         >
           <div
             className="line-clamp-9"
-            dangerouslySetInnerHTML={{ __html: stringWithLinks }}
           />
         </div>
       )}
